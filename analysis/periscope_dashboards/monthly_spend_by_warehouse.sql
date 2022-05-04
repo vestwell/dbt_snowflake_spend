308592e48865869fa98409aee465eca062e579ba
Robert Coombs <robert@idealfusion.com>
2022-05-04 17:10:27 -0400

    Updated models and dashboards

--- analysis/periscope_dashboards/monthly_spend_by_warehouse.sql
+++ analysis/periscope_dashboards/monthly_spend_by_warehouse.sql
@@ -2,8 +2,6 @@ SELECT  usage_month,
         warehouse_name,
         sum(dollars_spent) as spend
 FROM {{ref('snowflake_warehouse_metering_xf')}}
-WHERE [usage_month=daterange]
-  AND usage_month < date_trunc('month', CURRENT_TIMESTAMP)::date
-  AND [warehouse_name=warehouse_name]
+WHERE usage_month < date_trunc('month', CURRENT_TIMESTAMP)::date
 GROUP BY 1, 2
 ORDER BY 1 DESC
